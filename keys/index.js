f082f56889e7c9425a106a6c33bd9d993009e14a
Alex <surfer.ru@mail.ru>
2019-08-22 13:11:57 +0400

    finished

--- keys/index.js
+++ keys/index.js
@@ -1,7 +1,5 @@
-module.exports = {
-    MONGODB_URI: '', //MONGO DB URL
-    SESSION_SECRET: '', //SECRET KEY SESSION
-    SENDGRID_API_KEY: '',// SENDGRID API KEY
-    EMAIL_FROM: '', //FROM EMAIL
-    BASE_URL: 'http://____' //BASE URL
+if(process.env.NODE_ENV === 'production') {
+    module.exports = require('./keys.prod')
+} else {
+    module.exports = require('./keys.dev')
 }
---
+++ keys/keys.prod.js
@@ -0,0 +1,7 @@
+module.exports = {
+    MONGODB_URI: process.env.MONGODB_URI, //MONGO DB URL
+    SESSION_SECRET: process.env.SESSION_SECRET, //SECRET KEY SESSION
+    SENDGRID_API_KEY: process.env.SENDGRID_API_KEY,// SENDGRID API KEY
+    EMAIL_FROM: process.env.EMAIL_FROM, //FROM EMAIL
+    BASE_URL: process.env.BASE_URL //BASE URL
+}
